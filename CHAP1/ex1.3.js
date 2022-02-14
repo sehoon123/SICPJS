25c60029d50af26db034ed3017d3288032a786b2
sehun <admin@sehuns-MacBook-Air.local>
2022-02-14 18:04:10 +0900

    first week

--- CHAP1/ex1.3.js
+++ CHAP1/ex1.3.js
@@ -1,10 +1,10 @@
 function sum(a, b, c) {
     return (a + b >= b + c && a + b >= a + c
-        ? a + b
+        ? a*a + b*b
         : b + c >= a + b && b + c >= a + c
-        ? b + c
-        : a + c
+        ? b*b + c*c
+        : a*a + c*c
     )
 }
 
-console.log(sum(1,6,1))
+// console.log(sum(1,6,1))
