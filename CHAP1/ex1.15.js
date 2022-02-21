0d885b1e9fb23a1e2682b572c6cef252540b394d
sehun <admin@sehuns-MacBook-Air.local>
2022-02-21 14:35:25 +0900

    third week

---
+++ CHAP1/ex1.15.js
@@ -0,0 +1,32 @@
+function cube(x) {
+    return x * x * x;
+}
+
+function p(x) {
+    return 3 * x - 4 * cube(x);
+}
+
+function sine(angle) {
+    return ! (abs(angle) > 0.1)
+    ? angle
+    : p(sine(angle / 3));
+}
+
+function abs(x) {
+    return x >= 0
+    ? x
+    : -x;
+}
+
+console.log(sine(12.15))
+
+//12.15
+//P called
+//4.05
+//1.35
+//0.45
+//0.15
+//0.05
+//
+//Q2
+//O(log(3) a)
