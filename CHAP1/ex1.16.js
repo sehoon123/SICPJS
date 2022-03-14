48a8200f318090254a12e82c6739d58df7a54519
sehoon123 <sehoon0519@naver.com>
2022-03-14 19:57:13 +0900

    changes

--- CHAP1/ex1.16.js
+++ CHAP1/ex1.16.js
@@ -3,11 +3,11 @@ function exp_iter(a, b, count) {
         ? a
         :count % 2 == 0
         ? exp_iter(a, b*b, count/2)
-        : b*exp_iter(a, b*b, (count-1)/2)
+        : exp_iter(b*a, b*b, (count-1)/2)
 }
 
 function exp(b, n) {
     return exp_iter(1, b, n)
 }
 
-// console.log(exp(10,4))
+console.log(exp(3,4))
