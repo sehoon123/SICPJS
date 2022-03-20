eb8c41d6da98720fcf05ab6c122dafe1c03c14ed
sehoon123 <sehoon0519@naver.com>
2022-03-20 21:56:25 +0900

    changes

--- CHAP1/ex1.29.js
+++ CHAP1/ex1.29.js
@@ -8,21 +8,32 @@ function cube(x) {
 
 
 
-function simpson(f, a, b, n){
-    result = 0
+function simpson(f, a, b, n, k, answer){
     h = (b-a)/n
-    for (i = 0; i <= n; i++) {
-        if (i === 0 || i === n) {
-            result += (h/3)*f(a+i*h)
-        } else {
-            if (i % 2 === 0) {
-                result += 2*(h/3)*f(a+i*h)
-            } else {
-                result += 4*(h/3)*f(a+i*h)
-            }
-        }
-    }
-    return result
+    return k === 0
+        ? simpson(f, a, b, n, k+1, answer + (h/3) * f(a+k*h))
+        : k === n
+        ? answer + (h/3) * f(a+k*h)
+        : k % 2 === 0
+        ? simpson(f, a, b, n, k+1, answer + 2*(h/3)*f(a+k*h))
+        : simpson(f, a, b, n, k+1, answer + 4*(h/3)*f(a+k*h))
 }
 
-console.log(simpson(cube, 0, 1, 100));
+// function simpson(f, a, b, n){
+//     result = 0
+//     h = (b-a)/n
+//     for (i = 0; i <= n; i++) {
+//         if (i === 0 || i === n) {
+//             result += (h/3)*f(a+i*h)
+//         } else {
+//             if (i % 2 === 0) {
+//                 result += 2*(h/3)*f(a+i*h)
+//             } else {
+//                 result += 4*(h/3)*f(a+i*h)
+//             }
+//         }
+//     }
+//     return result
+// }
+
+console.log(simpson(cube, 0, 1, 100, 0, 0));
